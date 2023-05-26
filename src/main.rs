1e4b9724d8ab145cde02e54c4e7e8c6158277d0b
Qube404 <qube404@protonmail.com>
2023-05-26 11:36:28 +1000

    Added constant ID variable and light comments

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,22 @@ use serenity::async_trait;
 use serenity::prelude::*;
 use serenity::model::gateway::Ready;
 
+// Hard coded my user ID as I don't intend this bot to message anyone else.
+const QUBE_ID: u64 = 266059446159933452;
+
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, data: Ready) {
+    // Using the ready event as it is triggered as soon as the bot connects.
+    // The bot should always be ready to send a message from the twitch api
+    // so it starts as soon as the bot is connected as opposed to waiting 
+    // for some discord reliant event.
+    async fn ready(&self, ctx: Context, _data: Ready) {
         println!("Connected");
         let user = ctx
             .http
-            .get_user(266059446159933452)
+            .get_user(QUBE_ID)
             .await
             .expect("Expected a valid user");
 
